2c9027ea2a02cbf05f959995a7da3003241d3f59
Luke Wilson <asmoaesl@gmail.com>
2019-04-23 19:26:54 -0500

    Starting plans

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
+enum Token {
+    Let,
+    Eq,
+    Print,
+    Is,
+    ID(String),
+    Str(String),
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn tokenize(input: &str) -> Vec<Token> {
+    let mut chars = input.chars().peekable();
+
+    let mut tokens = Vec::new();
+
+    while let Some(c) = chars.next() {
+        match c {
+            _ => {}
+        }
+    }
+
+    tokens
 }
